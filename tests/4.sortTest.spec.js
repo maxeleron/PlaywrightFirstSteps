d747fb9db2a87652779b8809a61f4e38bbeaf416
Ivan Chepurkin <maxeleron@gmail.com>
2021-09-18 16:44:57 +0300

    Completed 4th test

--- tests/4.sortTest.spec.js
+++ tests/4.sortTest.spec.js
@@ -1,9 +1,36 @@
 const { test, expect } = require('@playwright/test');
 
-// fourth test
-// 
-test('fourth test', async ({ page }) => {
-    await page.goto('https://playwright.dev/');
-    const title = page.locator('.navbar__inner .navbar__title');
-    await expect(title).toHaveText('Playwright');
+test('User can sort elements', async ({ page }) => {
+    // Go to main page
+    await page.goto('https://rozetka.com.ua/');
+
+    // Open Catalogue ("Каталог")
+    await page.click('#fat-menu');
+
+    // The user clicks 'Ноутбуки и компьютеры' section
+    // await page.waitForSelector('text=Ноутбуки и компьютеры');
+    await page.click('text=Ноутбуки и компьютеры');
+
+    // 
+    await page.click('[title="Мониторы"]');
+
+    // Sort only
+    // Sort by brand (Dell)
+    await page.waitForSelector('[for="Dell"]');
+    await page.click('[for="Dell"]');
+
+    // Select "P" series
+    await page.waitForSelector('[for="P"]');
+    await page.click('[for="P"]');
+
+    // Select 24" - 24.9" diagonal
+    await page.waitForSelector(`[for='24" - 24.9"']`);
+    await page.click(`[for='24" - 24.9"']`);
+
+    // Wait for sort applied
+    await page.waitForSelector('.goods-tile__heading');
+
+    // Assertion to check that sorting applied correctly
+    const title = await page.locator('.goods-tile__heading').textContent();
+    await expect(title.trim()).toStrictEqual('Монитор 24.1" Dell P2421 Black (210-AWLE)');
   });
--- tests/5.shopPathTest.spec.js
+++ tests/5.shopPathTest.spec.js
@@ -1,9 +1,8 @@
 const { test, expect } = require('@playwright/test');
 
-// fifth test
-// 
-test('fifth test', async ({ page }) => {
-    await page.goto('https://playwright.dev/');
+test('User can perform a purchase', async ({ page }) => {
+    // Go to main page
+    await page.goto('https://rozetka.com.ua/');
     const title = page.locator('.navbar__inner .navbar__title');
     await expect(title).toHaveText('Playwright');
   });
