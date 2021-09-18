17afe33ef5517895555c4aabfd9b6485a0b85290
Ivan Chepurkin <maxeleron@gmail.com>
2021-09-18 18:49:11 +0300

    Added 5th test

--- tests/5.shopPathTest.spec.js
+++ tests/5.shopPathTest.spec.js
@@ -1,8 +1,27 @@
 const { test, expect } = require('@playwright/test');
 
-test('User can perform a purchase', async ({ page }) => {
+  test('User can perform a purchase', async ({ page }) => {
     // Go to main page
     await page.goto('https://rozetka.com.ua/');
-    const title = page.locator('.navbar__inner .navbar__title');
-    await expect(title).toHaveText('Playwright');
+
+    await page.fill('rz-header input', 'Наушники JBL Tune 500 черный');
+
+    // Click "Search" ("Найти") button
+    await page.click('rz-header form > button');
+
+    // Wait for tiles appear after search
+    await page.waitForSelector('.goods-tile');
+
+    // Click first headphones from search results
+    await page.click('.goods-tile');
+
+    // Click Buy button
+    await page.click('.product__buy .buy-button');
+
+    const shoppingCartLocator = page.locator('text=Корзина');
+    const purchaseButtonLocator = page.locator('text=" Оформить заказ "');
+
+    // Assertion to check that sorting applied correctly
+    await expect(shoppingCartLocator).toBeVisible();
+    await expect(purchaseButtonLocator).toBeVisible();
   });
