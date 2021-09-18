9a0b24789d3fd2b4dea16d436f4b5cfa74f5125a
Max Ponomarenko <maxeleron@gmail.com>
2021-09-18 13:59:39 +0300

    work on task

--- tests/3.searchTest.spec.js
+++ tests/3.searchTest.spec.js
@@ -3,7 +3,25 @@ const { test, expect } = require('@playwright/test');
 // third test
 // 
 test('third test', async ({ page }) => {
-    await page.goto('https://playwright.dev/');
-    const title = page.locator('.navbar__inner .navbar__title');
-    await expect(title).toHaveText('Playwright');
+    // Go to page of phone "Samsung Galaxy M32"
+    await page.goto('https://rozetka.com.ua/');
+    // Text input
+    await page.fill('rz-header input', 'Xiomi');
+
+    await page.click('rz-header form > button');
+
+    await page.waitForSelector('.goods-tile');
+
+    const goodsTiles = await page.$$('.goods-tile__title');
+
+    // let firstElm = await page.locator(goodsTiles[0]);
+    await expect(goodsTiles[0].textContent()).toStrictEqual('some');
+    // await expect(firstElm.textContent()).toStrictEqual('some');
+
+    // goodsTiles.map(async(item) => await expect(item).toHaveText('Xiomi'));
+    // const se = page.locator('.product__buy .buy-button');
+    // const buyCreditButton = page.locator('.product__credit .button--large');
+
+    // await expect(buyButton).toHaveCSS('background-color', 'rgb(0, 160, 70)');
+    // await expect(buyCreditButton).toHaveCSS('background-color', 'rgb(62, 119, 170)');
   });
