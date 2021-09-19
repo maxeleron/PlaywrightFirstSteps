f8a83a6b5eb54a298df7638d5620a4638532e90c
Ivan Chepurkin <maxeleron@gmail.com>
2021-09-19 13:47:15 +0300

    some

--- tests/1.textAssertion.spec.js
+++ tests/1.textAssertion.spec.js
@@ -1,9 +1,9 @@
 const { test, expect } = require('@playwright/test');
 
-// first test
 // Assertion for text
-test('first test', async ({ page }) => {
+test('Catalogue button has correct text', async ({ page }) => {
     await page.goto('https://rozetka.com.ua/');
+
     const title = await page.locator('#fat-menu').textContent();
     await expect(title.trim()).toStrictEqual('Каталог');
   });
--- tests/4.sortTest.spec.js
+++ tests/4.sortTest.spec.js
@@ -12,25 +12,30 @@ test('User can sort elements', async ({ page }) => {
     await page.click('text=Ноутбуки и компьютеры');
 
     // 
-    await page.click('[title="Мониторы"]');
+    await page.click('[title="Ноутбуки"]');
 
     // Sort only
     // Sort by brand (Dell)
-    await page.waitForSelector('[for="Dell"]');
-    await page.click('[for="Dell"]');
+    await page.waitForSelector('[for="Apple"]');
+    await page.click('[for="Apple"]');
 
+    // Sort by brand (Dell)
+    await page.waitForSelector('[for="MacBook Pro 2020"]');
+    await page.click('[for="MacBook Pro 2020"]');
+
+    // await page.waitForTimeout(1000);
     // Select "P" series
-    await page.waitForSelector('[for="P"]');
-    await page.click('[for="P"]');
+    await page.waitForSelector('[for="Есть в наличии"]');
+    await page.click('[for="Есть в наличии"]');
 
     // Select 24" - 24.9" diagonal
-    await page.waitForSelector(`[for='24" - 24.9"']`);
-    await page.click(`[for='24" - 24.9"']`);
+    // await page.waitForSelector(`[for='24" - 24.9"']`);
+    // await page.click(`[for='24" - 24.9"']`);
 
     // Wait for sort applied
     await page.waitForSelector('.goods-tile__heading');
 
     // Assertion to check that sorting applied correctly
     const title = await page.locator('.goods-tile__heading').textContent();
-    await expect(title.trim()).toStrictEqual('Монитор 24.1" Dell P2421 Black (210-AWLE)');
+    await expect(title.trim()).toStrictEqual('Б/у Tower / Athlon II X2 240 (2 ядра по 2.8 GHz) / 4 GB DDR3 / 320 GB HDD / 400W');
   });
